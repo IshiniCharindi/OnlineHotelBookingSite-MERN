99d5aca21354fba9e2b91b7f31bef994fced28d1
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-01-16 16:03:15 +0530

    istall some depedencies

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./Components/User Section/Home page/Home.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+        <Home/>
     </>
   )
 }
